2a420b0f68175200f173fbd18fa77d1fcad23789
Bảo Nguyên <baonguyenyam@gmail.com>
2018-05-06 16:12:33 +0700

    banner

---
+++ CANHCAM-LIB/banner/b-1/index.js
@@ -0,0 +1,24 @@
+$(function() {
+    if ($('.canhcam-banner-1 .owl-carousel').length) {
+		$('.canhcam-banner-1 .owl-carousel').owlCarousel({
+            items: 1,
+            loop: true,
+            center: true,
+            padding: 0,
+            margin: 0,
+            navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>'],
+            nav: false,
+			dots: true,
+            autoplay: true,
+            autoplayTimeout: 5000,
+			autoplayHoverPause: true,
+			callbacks: true,
+			responsive: {
+				768: {
+					nav: true
+				}
+			}
+        });
+    }
+
+});
