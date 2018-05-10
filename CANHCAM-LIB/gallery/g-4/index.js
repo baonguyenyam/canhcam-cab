5a2e5d005e01915b423eebd05323c06ac2d2667e
Bảo Nguyên <baonguyenyam@gmail.com>
2018-05-10 15:41:35 +0700

    fixed bug

---
+++ CANHCAM-LIB/gallery/g-4/index.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+
+	$(".canhcam-gallery-4 .gallery-details .gallery-lists").lightGallery({
+		width: '700px',
+		height: '470px',
+		mode: 'lg-fade',
+		addClass: 'canhcam-gallery-4 canhcam-video-fixed-size',
+		counter: false,
+		download: false,
+		startClass: '',
+		enableSwipe: false,
+		enableDrag: false,
+		speed: 500,
+		thumbnail: true,
+		animateThumb: false,
+		showThumbByDefault: false,
+		selector: '.item'
+	});
+
+});
