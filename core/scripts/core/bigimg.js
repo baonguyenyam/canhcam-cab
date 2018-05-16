5dac62d51f132be1aab0a632a09090706322d4ef
Bảo Nguyên <baonguyenyam@gmail.com>
2018-05-16 23:49:38 +0700

    up docs

--- core/scripts/core/bigimg.js
+++ core/scripts/core/bigimg.js
@@ -1,62 +1,80 @@
 function CanhCamResponsive() {
-    // Set BG Resposive
-    $('[bg-img]').each(function() {
-        var bgimg = $(this).attr('bg-img');
-        $(this).css({
-            "background-image": "url(" + bgimg + ")",
-            "background-position": "center center",
-            "background-size": "cover"
-        });
-    });
+	// Set BG Resposive
+	$('[bg-img]').each(function () {
+		var bgimg = $(this).attr('bg-img');
+		var pos = $(this).attr('bg-pos');
+		var size = $(this).attr('bg-size');
+		if (pos && pos.length > 0) {
+			$(this).css({
+				"background-position": pos
+			});
+		} else {
+			$(this).css({
+				"background-position": "center center"
+			});
+		}
+		if (size && size.length > 0) {
+			$(this).css({
+				"background-size": size
+			});
+		} else {
+			$(this).css({
+				"background-size": "cover"
+			});
+		}
+		$(this).css({
+			"background-image": "url(" + bgimg + ")"
+		});
+	});
 
-    $('[bg-img-responsive]').each(function() {
-        var bgimg = $(this).attr('bg-img-responsive');
-        var bgimgsm = $(this).attr('bg-img-responsive-sm');
-        var bgimgxs = $(this).attr('bg-img-responsive-xs');
-        if ($(window).width() >= CANHCAM_APP.CHANGE_GRID) {
-            $(this).css({
-                "background-image": "url(" + bgimg + ")",
-                "background-position": "center center",
-                "background-size": "cover"
-            });
+	$('[bg-img-responsive]').each(function () {
+		var bgimg = $(this).attr('bg-img-responsive');
+		var bgimgsm = $(this).attr('bg-img-responsive-sm');
+		var bgimgxs = $(this).attr('bg-img-responsive-xs');
+		if ($(window).width() >= CANHCAM_APP.CHANGE_GRID) {
+			$(this).css({
+				"background-image": "url(" + bgimg + ")",
+				"background-position": "center center",
+				"background-size": "cover"
+			});
 		} else if ($(window).width() < CANHCAM_APP.CHANGE_GRID && $(window).width() > CANHCAM_APP.CHANGE_GRID_SM) {
-            $(this).css({
-                "background-image": "url(" + bgimgsm + ")",
-                "background-position": "center center",
-                "background-size": "cover"
-            });
-        } else {
-            $(this).css({
-                "background-image": "url(" + bgimgxs + ")",
-                "background-position": "center center",
-                "background-size": "cover"
-            });
-        }
-    });
+			$(this).css({
+				"background-image": "url(" + bgimgsm + ")",
+				"background-position": "center center",
+				"background-size": "cover"
+			});
+		} else {
+			$(this).css({
+				"background-image": "url(" + bgimgxs + ")",
+				"background-position": "center center",
+				"background-size": "cover"
+			});
+		}
+	});
 
-    $('[img-src-responsive]').each(function() {
-        var bgimg2 = $(this).attr('img-src-responsive');
-        var bgimg2sm = $(this).attr('img-src-responsive-sm');
-        var bgimg2xs = $(this).attr('img-src-responsive-xs');
-        if ($(window).width() >= CANHCAM_APP.CHANGE_GRID) {
-            $(this).attr("src", "" + bgimg2 + "");
+	$('[img-src-responsive]').each(function () {
+		var bgimg2 = $(this).attr('img-src-responsive');
+		var bgimg2sm = $(this).attr('img-src-responsive-sm');
+		var bgimg2xs = $(this).attr('img-src-responsive-xs');
+		if ($(window).width() >= CANHCAM_APP.CHANGE_GRID) {
+			$(this).attr("src", "" + bgimg2 + "");
 		} else if ($(window).width() < CANHCAM_APP.CHANGE_GRID && $(window).width() > CANHCAM_APP.CHANGE_GRID_SM) {
-            $(this).attr("src", "" + bgimg2sm + "");
-        } else {
-            $(this).attr("src", "" + bgimg2xs + "");
-        }
-    });
+			$(this).attr("src", "" + bgimg2sm + "");
+		} else {
+			$(this).attr("src", "" + bgimg2xs + "");
+		}
+	});
 
 };
 
-$(function() {
-    if (CANHCAM_APP.ACTIVE_RESPONSIVE) {
-        CanhCamResponsive();
-    }
+$(function () {
+	if (CANHCAM_APP.ACTIVE_RESPONSIVE) {
+		CanhCamResponsive();
+	}
 })
 
-$(window).resize(function() {
-    if (CANHCAM_APP.ACTIVE_RESPONSIVE) {
-        CanhCamResponsive();
-    }
+$(window).resize(function () {
+	if (CANHCAM_APP.ACTIVE_RESPONSIVE) {
+		CanhCamResponsive();
+	}
 })
