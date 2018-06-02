927628c3f8305f84a879b3db9dc3333933357079
Bảo Nguyên <baonguyenyam@gmail.com>
2018-06-02 23:05:59 +0700

    add JS

--- core/scripts/core/id.js
+++ core/scripts/core/id.js
@@ -6,3 +6,15 @@ function canhcamID(e) {
 	return text;
 }
 
+function b64EncodeUnicode(str) {
+	return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g,
+		function toSolidBytes(match, p1) {
+			return String.fromCharCode('0x' + p1);
+		}));
+}
+
+function b64DecodeUnicode(str) {
+	return decodeURIComponent(atob(str).split('').map(function (c) {
+		return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+	}).join(''));
+}
