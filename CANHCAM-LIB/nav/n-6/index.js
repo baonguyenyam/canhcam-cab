37f319c50793d72fed46784fc618d49aae062e62
Bảo Nguyên <baonguyenyam@gmail.com>
2018-06-07 14:53:27 +0700

    up

---
+++ CANHCAM-LIB/nav/n-6/index.js
@@ -0,0 +1,23 @@
+$(function() {
+    if ($('.canhcam-nav-6 .owl-carousel').length) {
+		$('.canhcam-nav-6 .owl-carousel').owlCarousel({
+            items: 1,
+            loop: false,
+            center: true,
+            padding: 0,
+            margin: 0,
+            navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>'],
+            nav: false,
+			dots: false,
+            autoplay: false,
+            autoplayTimeout: 5000,
+			autoplayHoverPause: true,
+			callbacks: true,
+			responsive: {
+				768: {
+				}
+			}
+        });
+    }
+
+});
