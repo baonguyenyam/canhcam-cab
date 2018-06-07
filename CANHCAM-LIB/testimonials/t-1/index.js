3cbf2413573535cf64d0c25b4143767db0472021
Bảo Nguyên <baonguyenyam@gmail.com>
2018-06-07 21:09:53 +0700

    testimonial

--- CANHCAM-LIB/testimonials/t-1/index.js
+++ CANHCAM-LIB/testimonials/t-1/index.js
@@ -1,8 +1,21 @@
-
-$(document).ready(function() {
+$(document).ready(function () {
+	$('.canhcam-testimonials-1 .owl-carousel').owlCarousel({
+		items: 1,
+		loop: true,
+		center: false,
+		padding: 10,
+		margin: 20,
+		navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>'],
+		nav: false,
+		dots: true,
+		autoplay: true,
+		autoplayTimeout: 3000,
+		autoplayHoverPause: false,
+		callbacks: true
+	});
 
 });
 
-$(window).resize(function() {
+$(window).resize(function () {
 
 })
