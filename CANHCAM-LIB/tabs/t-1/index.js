d328b91594de74737c61f81afa3e55540815464d
Bảo Nguyên <baonguyenyam@gmail.com>
2018-06-09 00:41:27 +0700

    tabs

--- CANHCAM-LIB/tabs/t-1/index.js
+++ CANHCAM-LIB/tabs/t-1/index.js
@@ -1,10 +1,12 @@
 
-function checkTab2() {
+function checkTab1() {
+	$('.canhcam-tabs-1 #cct-01-tab').tab('show').parents('li').addClass('active')
 	$('.canhcam-tabs-1 a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
-		console.log(e)
+		$(this).parents('ul').find('li').removeClass('active')
+		$(this).parents('li').addClass('active')
 	})
 }
 
 $(document).ready(function () {
-	checkTab2()
+	checkTab1()
 });
