8b0fcc3354875278fa8beeb51ec0ef9fced3f786
Bảo Nguyên <baonguyenyam@gmail.com>
2018-06-10 01:11:25 +0700

    add menu

---
+++ CANHCAM-LIB/nav/n-7/index.js
@@ -0,0 +1,10 @@
+
+function ccToggleMenuIcons7() {
+	$(".canhcam-nav-7 .floating-options").on("click", function () {
+		$(this).toggleClass("active-tgm");
+	});
+ }
+
+$(document).ready(function () {
+	ccToggleMenuIcons7()
+});
