2f247c99a30588568bb08977cd23bfaaa8514ef1
Bảo Nguyên <baonguyenyam@gmail.com>
2018-06-29 16:28:47 +0700

    fixed box

---
+++ CANHCAM-LIB/boxes/b-16/index.js
@@ -0,0 +1,42 @@
+function getBG16(time) {
+	$('.canhcam-boxes-16 .item').each(function () {
+		var newgt = $(this).find('figcaption p').innerHeight()
+		$(this).find('figcaption p').css({
+			"height": "0px",
+			"overflow": "hidden"
+		})
+		var em = $(this).find('figure').innerHeight()
+		var newg = $(this).find('figcaption').innerHeight()
+		$(this).hover(function () {
+			$(this).find('figcaption').animate({
+				height: em + "px"
+			}, {
+				duration: time,
+				complete: function () {}
+			})
+			$(this).find('figcaption p').animate({
+				height : newgt + "px"
+			}, {
+				duration: time,
+				complete: function () {}
+			})
+		  }, function () {
+			$(this).find('figcaption').animate({
+				height : newg + "px"
+			}, {
+				duration: time,
+				complete: function () {}
+			})
+			$(this).find('figcaption p').animate({
+				height : "0px"
+			}, {
+				duration: time,
+				complete: function () {}
+			})
+		  });
+	})
+}
+
+$(document).ready(function () {
+	getBG16(1000)
+});
