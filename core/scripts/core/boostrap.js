0edfcad0f2642dfac6ce86353a305ce0f858609a
Bảo Nguyên <baonguyenyam@gmail.com>
2018-06-29 22:53:57 +0700

    fixed header

--- core/scripts/core/boostrap.js
+++ core/scripts/core/boostrap.js
@@ -4,3 +4,15 @@ $(function() {
         trigger: 'focus'
     })
 })
+// Thêm [dropdown-type="hover"] vào thẻ a để bật tính năng hover open dropdown 
+if ($(window).width() > CANHCAM_APP.CHANGE_GRID) {
+	$('.dropdown').on('mouseenter mouseleave', function () {
+		var ___d = $(this).find('[dropdown-type="hover"]').parents('.dropdown')
+		if (___d && ___d.length>0) {
+			setTimeout(function () {
+				___d[___d.is(':hover') ? 'addClass' : 'removeClass']('show');
+				___d.is(':hover') ? ___d.find('.dropdown-menu').show() : ___d.find('.dropdown-menu').hide()
+			}, 10);
+		}
+	})
+}
