5ff0017058b8e7e0ad753e2e4015c2f44958664b
Bảo Nguyên <baonguyenyam@gmail.com>
2018-07-02 10:11:01 +0700

    fixed JS

--- core/scripts/core/boostrap.js
+++ core/scripts/core/boostrap.js
@@ -8,7 +8,7 @@ $(function() {
 if ($(window).width() > CANHCAM_APP.CHANGE_GRID) {
 	$('.dropdown').on('mouseenter mouseleave', function () {
 		var ___d = $(this).find('[dropdown-type="hover"]').parents('.dropdown')
-		if (___d && ___d.length>0) {
+		if (___d && ___d.length > 0) {
 			$(this).find('[dropdown-type="hover"]').removeAttr('data-toggle')
 			setTimeout(function () {
 				___d[___d.is(':hover') ? 'addClass' : 'removeClass']('show');
@@ -16,4 +16,7 @@ if ($(window).width() > CANHCAM_APP.CHANGE_GRID) {
 			}, 10);
 		}
 	})
+} else {
+	$('.dropdown .dropdown-menu').show()
 }
+
